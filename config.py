2b2f7d759f107523086796904eb95e902ab5a2ab
HappyHippo77 <badpiggies927@gmail.com>
2024-01-07 22:08:33 -0500

    Finish setting up bot

--- config.py
+++ config.py
@@ -10,7 +10,6 @@ with open('token.txt', 'r') as file:
 token = token.strip()
 description = "I think I'm a useful bot."
 version = "1.0.0"
-repository = "https://github.com/Kelutral-org/ewo-bot"
-test_server_link = "https://discord.gg/B9rT2mHJTW"
+repository = "https://github.com/HappyHippo77/paytsyip"
 discord_library = "Disnake"
-operators = [423581502970789889, 189504650645471232, 205370567614922753, 429361033446948864, 81105065955303424]
+operators = []
